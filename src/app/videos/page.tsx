1a76213ca7466e6eec7477911c861be16f195bd4
Gabriellocp <gabriel_campista@hotmail.com>
2025-04-04 16:18:59 -0300

    feat: Add empty videos validation

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,10 +1,35 @@
 "use client";
+import { Button } from "@/components/Button";
 import { VideoList } from "@/components/VideoList";
 import { VideoWrapper } from "@/components/VideoWrapper";
 import { useHeaderChange } from "@/hooks/useHeaderChange";
+import { useVideoContext } from "@/providers/VideoProvider";
+import { useRouter } from "next/navigation";
 
 export default function Video() {
   useHeaderChange("My videos", "/");
+  const { videos } = useVideoContext();
+  const router = useRouter();
+  const handleAddVideo = () => {
+    router.push("upload");
+  };
+  if (videos.length === 0) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 24,
+          alignItems: "center",
+          margin: "auto",
+          placeContent: "center",
+        }}
+      >
+        <h1>Oops... It looks like you haven't add any videos yet.</h1>
+        <Button onClick={handleAddVideo}>New video</Button>
+      </div>
+    );
+  }
   return (
     <div
       style={{
